841e5dd90e4a51bc7f1cd0b78e1ad99bc87126a6
Sh1rin <blahut98@gmail.com>
2023-06-11 21:07:05 +0200

    Navbar fix
    add active navbar links.

--- Palinka/Script.js
+++ Palinka/Script.js
@@ -10,3 +10,22 @@ const toggleBtn = document.querySelector('.toggle_btn')
                     ? 'fa-solid fa-xmark'
                     : 'fa-solid fa-bars'
             }
+
+let sections = document.querySelectorAll("section")
+let navLinks = document.querySelectorAll("header div ul li a")
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop -60
+        let height = sec.offsetHeight
+        let id = sec.getAttribute("id")
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("active")
+                document.querySelector("header div ul li [href*=" + id + "]").classList.add("active")
+            })
+        }
+    })
+}
