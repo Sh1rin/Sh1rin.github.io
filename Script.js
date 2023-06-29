b2052c933a171cb4900cb999e7439301ba96a0e3
Sh1rin <blahut98@gmail.com>
2023-06-29 23:21:50 +0200

    navbar and css fixes
    fixed about page section
    refactored JavaScript code
    Navbar fixes
    added responsive features

--- Script.js
+++ Script.js
@@ -1,15 +1,22 @@
 const toggleBtn = document.querySelector('.toggle_btn')
-            const toggleBtnIcon = document.querySelector('.toggle_btn i')
-            const dropDownMenu = document.querySelector('.dropdown_menu')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
 
-            toggleBtn.onclick = function () {
-                dropDownMenu.classList.toggle('open')
-                const isOpen = dropDownMenu.classList.contains('open')
+function toggleNavbar() {
+    const dropDownMenu = document.querySelector('.dropdown_menu')
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+    isOpen ? document.querySelector('.navbar').classList.add('opened')
+        : document.querySelector('.navbar').classList.remove('opened')
+    toggleBtnIcon.classList = isOpen
+                ? 'fa-solid fa-xmark'
+                : 'fa-solid fa-bars'
+    
+}
 
-                toggleBtnIcon.classList = isOpen
-                    ? 'fa-solid fa-xmark'
-                    : 'fa-solid fa-bars'
-            }
+toggleBtn.onclick = function () {
+    toggleNavbar()
+}
+
 
 let sections = document.querySelectorAll("section")
 let navLinks = document.querySelectorAll("header div ul li a")
@@ -29,3 +36,23 @@ window.onscroll = () => {
         }
     })
 }
+
+const arrowContainer = document.querySelector('.arrow-container');
+
+arrowContainer.addEventListener('click', () => {
+  const screenHeight = window.innerHeight;
+  window.scrollTo({
+    top: screenHeight,
+    behavior: 'smooth'
+  });
+});
+
+/*hide on click*/
+
+let div = document.getElementsByClassName('close');
+for (let i = 0; i < div.length; i++) {
+    div[i].addEventListener('click', function(e){
+        toggleNavbar()
+      });
+}
+
